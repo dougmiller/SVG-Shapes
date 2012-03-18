6d3788f259409f31222d911cc180b55d8d01e3a0
Douglas Miller <dougmiller@themetacity.com>
2012-03-18 11:01:37 +1000

    Now added in the objects I want, but I dont know how to workout if fully pared yet.

--- script.js
+++ script.js
@@ -1,60 +1,78 @@
-function makeAJAXCall(hash, cb) {
-    var returnedJSON, cb = cb, hash = hash;
-    $.ajax({
-        accepts: 'application/vnd.github-blob.raw',
-        dataType: 'jsonp',
-        url: hash,
-        success: function (json) {
-            console.info(json);
-            returnedJSON = json;
-			
-			// Time for callback to be executed
-            if (cb) {
-				cb(json);
-			}
-        },
-        error: function (error) {
-            console.error(error);
-            // an error happened, check it out.
-            throw error;
-        }
-    });
-    return returnedJSON;
-}
+(function () {
+    'use strict';
 
-function parseBlob(hash) {
-    var objectedJSON, objectList = [], i;
-    objectedJSON = makeAJAXCall(hash, function (objectedJSON) {  // no loop as only one entry
-        objectList.push(objectedJSON.content);
-    });
-    return objectList;
-}
+    var objectsList = [];
 
-function walkTree(hash) {
-    var objectedJSON, objectList = [], i, entry;
-    var hash = 'https://api.github.com/repos/DougMiller/SVG-Shapes/git/trees/' + hash;
-    objectedJSON = makeAJAXCall(hash, function (objectedJSON) {
-        for (i = 0;  i < objectedJSON.data.tree.length; i += 1) {
-            entry = objectedJSON.data.tree[i];
-            console.debug(entry);
-            if (entry.type === 'blob') {
-                if (entry.path.slice(-4) === '.svg') {     // we only want the svg images not the ignore file and README etc
-                    console.info(entry.path)
-                    objectList.push(parseBlob(entry.url));
+    function makeAJAXCall(hash, cb) {
+        $.ajaxSetup({
+            accept: 'application/vnd.github.raw',
+            dataType: 'jsonp'
+        });
+
+        $.ajax({
+            url: hash,
+            success: function (json) {
+
+                if (cb) {
+                    cb(json);
                 }
-            } else if (entry.type === 'tree') {
-                objectList.push(walkTree(entry.sha));
+            },
+            error: function (error) {
+                console.error(error);
+                throw error;
             }
+        });
+    }
+
+    function parseBlob(hash, cb) {
+        makeAJAXCall(hash, function (returnedJSON) {  // no loop as only one entry
+            if (cb) {
+                cb(returnedJSON.data);
+            }
+        });
+    }
+
+    function complete(loopLength, treeContents, cb) {
+
+        if (cb && loopLength === 0) {
+            objectsList.push(treeContents);
+            cb();
         }
-    
-    });
-    console.info(objectList);
-    return objectList;
-}
+    }
 
-$(document).ready(function () {
-    var objects = walkTree('master', function () {     // master to start at the top and work our way down
-        console.info(objects);
-    });
-});
+    function parseTree(hash, treeName, cb) {
+        var treeContents = {'tree': treeName, 'blobs': []}, loopLength, i, entry;
+        var tree = 'https://api.github.com/repos/DougMiller/SVG-Shapes/git/trees/' + hash;
 
+        makeAJAXCall(tree, function (returnedJSON) {
+            loopLength = returnedJSON.data.tree.length;
+            for (i = 0;  i < returnedJSON.data.tree.length; i += 1) {
+
+                entry = returnedJSON.data.tree[i];
+
+                if (entry.type === 'blob') {
+                    if (entry.path.slice(-4) === '.svg') {     // we only want the svg images not the ignore file and README etc
+                        parseBlob(entry.url, function (json) {
+                            treeContents.blobs.push(json.content);
+                            loopLength -= 1;
+                            complete(loopLength, treeContents, cb);
+                        });
+                    } else {
+                        loopLength -= 1;  // extra files we don't care about
+                    }
+                } else if (entry.type === 'tree') {
+                    parseTree(entry.sha, entry.path, function () {
+                        console.info(objectsList);
+                    });
+                    loopLength -= 1;
+                }
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        parseTree('master', 'master', function () {     // master to start at the top and work our way down
+            console.info(objectsList);
+        });
+    });
+}());
